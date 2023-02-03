211b7d2383e028111ead61b99685d9db0afeef0a
brianjo94 <brianjo1994@gmail.com>
2023-02-02 23:36:01 -0500

    Practiced JS exercises with description in comments

---
+++ beginner_JS_exercises/exercise1/js.js
@@ -0,0 +1,9 @@
+//Write a JS program to check two numbers and return true if one of the number is 100 or if sum of two numbers is 100
+
+
+
+const isEqualTo100 = (a,b) => a === 100 || b === 100 || (a + b ===100);
+
+
+console.log(isEqualTo100(100,85));
+
---
+++ beginner_JS_exercises/exercise2/js.js
@@ -0,0 +1,10 @@
+//write a JS program to get the extension of filename
+
+
+const message = 'hello'
+const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));
+
+
+console.log(getFileExtension('index.htmld'));
+
+console.log(lastIndexOf(message))
